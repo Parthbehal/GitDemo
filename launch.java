0f8906ff2dd4b963d41afebfbfbcd384ce6c6a12
Parth <Parthbehal34@gmail.com>
2025-05-17 13:31:25 +0530

    Modified After cloning

--- launch.java
+++ launch.java
@@ -2,6 +2,14 @@ class{
        public static void main(Strings[] args){
                  System.out.println("Exploring Git and Github");
 
-}
 
+}
+     public static void fun()
+     {
+       int a=10;
+       int b=20;
+       int sum=a+b;
+       System.out.println(sum);
+     }
+  
 }
